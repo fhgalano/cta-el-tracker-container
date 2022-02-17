329b9de903e0adc2886bbe35a0561098ce4e73fc
fhgalano <fhgalano@gmail.com>
2022-02-17 17:57:52 -0600

    refactored station searching functionality to be more readable

--- cta_map.py
+++ cta_map.py
@@ -1,30 +1,58 @@
 import pandas as pd
 
-cta_station_data = pd.read_csv('CTA_-_System_Information_-_List_of__L__Stops.csv')
+
+c_map = {
+    'RED': 'RED',
+    'BLUE': 'BLUE',
+    'GREEN': 'G',
+    'BROWN': 'BRN',
+    'PURPLE': 'P',
+    'PURPLE EXPRESS': 'Pexp',
+    'YELLOW': 'Y',
+    'PINK': 'PNK'
+}
 
 
-def get_stp_id(quadrant, color, direction, station_name):
-    possible_stations = cta_station_data
-    possible_stations = get_stations_with_name(possible_stations, station_name)
-    possible_stations = get_stations_with_color(possible_stations, color)
-    possible_stations = get_stations_in_direction(possible_stations, direction)
+def get_stp_id(quad, color, direction, stop_name):
+    # Process Inputs
+    color = color_map(color)
 
-    return possible_stations.STOP_ID
+    # Filter Mapping
+    route_map = pd.read_csv('CTA_-_System_Information_-_List_of__L__Stops.csv')
+    route_map = _get_entries_with_stop_name(stop_name, route_map)
+    route_map = _get_entries_of_color(color, route_map)
+    route_map = _get_entries_with_direction(direction, route_map)
+
+    # Verify Solution
+    _check_if_single_solution(route_map)
+
+    return route_map.STOP_ID.values[0]
 
 
-def get_stations_with_name(possible_stations, name):
-    return possible_stations[possible_stations.STATION_NAME.str.contains(name)]
+def color_map(color):
+    return c_map[color.upper()]
 
 
-def get_stations_with_color(possible_stations, color):
-    return possible_stations[possible_stations[color.upper()]]
+def _get_entries_with_stop_name(stop_name, route_map):
+    return route_map[route_map.STATION_NAME.str.contains(stop_name)]
 
 
-def get_stations_in_direction(possible_stations, direction):
-    return possible_stations[possible_stations.DIRECTION_ID.str.contains(direction)]
+def _get_entries_of_color(color, route_map):
+    return route_map[route_map[color]]
+
+
+def _get_entries_with_direction(direction, route_map):
+    return route_map[route_map.DIRECTION_ID.str.contains(direction)]
+
+
+def _check_if_single_solution(route_map):
+    if route_map.shape[0] != 1:
+        raise Exception("No Single Solution for Station ID")
+    else:
+        pass
 
 
 if __name__ == "__main__":
-    id = get_stp_id('we', 'Red', 'S', 'Belmont')
+    stp_id = get_stp_id('we', 'Red', 'S', 'Belmont')
 
-    print('debug')
+    print(stp_id)
--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from typing import Optional
 
 from fastapi import FastAPI
 from cta_requests import get_train_eta
+from cta_map import get_stp_id
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return "The App is Running"
 
-@app.get("train_eta/{quadrant}/{color}/{station_id}/{direction}")
-def read_train_eta(station_id: str)
+
+@app.get("/train_eta/{color}/{station_name}/{direction}")
+def read_train_eta(color: str, station_name: str, direction: str):
+    station_id = get_stp_id(None, color, direction, station_name)
+    print(station_id)
+    return get_train_eta(station_id)
