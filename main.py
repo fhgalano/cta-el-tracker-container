ddcab2cabe987df76c2846102e43ba0f2eb0f6d1
fhgalano <fhgalano@gmail.com>
2022-05-29 20:30:36 -0500

    updated to run app from call to ./main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from typing import Optional
-
 from fastapi import FastAPI
-from cta_requests import get_train_eta
+from uvicorn import run
+
 from cta_map import get_stp_id
+from cta_requests import get_train_eta
 
 app = FastAPI()
 
@@ -17,3 +17,9 @@ def read_train_eta(color: str, station_name: str, direction: str):
     station_id = get_stp_id(None, color, direction, station_name)
     print(station_id)
     return get_train_eta(station_id)
+
+# TODO: Fix the Brown Wellington South Case from delivering a purple line train
+
+
+if __name__ == "__main__":
+    run(app, host="0.0.0.0", port=5000)
